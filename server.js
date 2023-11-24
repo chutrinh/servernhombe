08ddaa0274e2af07e7472154d0422fe16da23f3a
chutrinh <125455926+chutrinh@users.noreply.github.com>
2023-11-24 20:35:17 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const mongoose = require("mongoose");
 // const authenRoutes = require("./routes/authen");
 // const clientRoutes = require("./src/routes/client/client.js");
 // const adminRoutes = require("./routes/admin");
-// const database = require("./src/config/db/database.js");
+const database = require("./src/config/db/database.js");
 // const multer = require("multer");
 const app = express();
 const port = 5000;
 app.use(cors());
 
-// database.connect();
+database.connect();
 // test
 // app.use("/api", clientRoutes);
 
