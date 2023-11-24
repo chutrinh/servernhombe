75e39e058212e5a04314497d2bdbfdd4313a38a0
chutrinh <125455926+chutrinh@users.noreply.github.com>
2023-11-24 20:39:23 +0700

    Update homeController.js

--- src/controllers/client/homeController.js
+++ src/controllers/client/homeController.js
@@ -1,11 +1,11 @@
-const MacBook = require("../../models/macBook");
-const TypeMacbook = require("../../models/typeMacbook");
+// const MacBook = require("../../models/macBook");
+// const TypeMacbook = require("../../models/typeMacbook");
 
-exports.getDatasHome = async (req, res, next) => {
-  MacBook.find()
-    .populate("type.productId")
-    .exec()
-    .then((data) => {
-      res.json(data);
-    });
-};
+// exports.getDatasHome = async (req, res, next) => {
+//   MacBook.find()
+//     .populate("type.productId")
+//     .exec()
+//     .then((data) => {
+//       res.json(data);
+//     });
+// };
