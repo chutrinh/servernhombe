c10781ad701faea68a4150b0ef5beafccb957e03
chutrinh <125455926+chutrinh@users.noreply.github.com>
2023-11-24 20:48:06 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,18 +2,14 @@ const path = require("path");
 const cors = require("cors");
 const express = require("express");
 const bodyParser = require("body-parser");
-// const mongoose = require("mongoose");
-// const authenRoutes = require("./routes/authen");
-// const clientRoutes = require("./src/routes/client/client.js");
-// const adminRoutes = require("./routes/admin");
-// const database = require("./src/config/db/database.js");
-// const multer = require("multer");
+const mongoose = require("mongoose");
+const clientRoutes = require("./src/routes/client/client.js");
 const app = express();
 const port = 5000;
 app.use(cors());
 
-// database.connect();
-// test
-// app.use("/api", clientRoutes);
+app.use("/api", clientRoutes);
 
-app.listen(process.env.PORT||port);
+mongoose.connect("mongodb+srv://trinhtvfx22649:DxxTUGzQ9ivnJ3Kr@cluster0.ssvrvkk.mongodb.net/nhombe").then(()=>{
+  app.listen(process.env.PORT||port);
+})
